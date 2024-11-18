c4452d612d36db7e644f1bbaaa880001f064c7c2
Gabriel Hernandez <leirbagisaihf@gmail.com>
2024-11-17 19:51:20 -0600

    Se agrega el SGA

---
+++ SGA/js/script.js
@@ -0,0 +1,20 @@
+// Manejar el inicio de sesión
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    document.getElementById('login-page').classList.add('d-none');
+    document.getElementById('main-page').classList.remove('d-none');
+});
+
+// Manejar la navegación de las secciones
+const links = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('.content-section');
+
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const sectionId = link.getAttribute('data-section');
+        
+        sections.forEach(section => section.classList.add('d-none'));
+        document.getElementById(sectionId).classList.remove('d-none');
+    });
+});
